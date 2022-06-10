cd55659bdbd8117868c2617693d0af5d20fd1cab
Noobsdev2k <99718570+Noobsdev2k@users.noreply.github.com>
2022-06-11 01:32:43 +0700

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
 import "./App.scss";
 
 function App() {
-  return <div className="App">hello world!!!</div>;
+  return (
+    <div className="App">
+      hello world!!!
+      <>hung dep trai</>
+      <div>test</div>
+    </div>
+  );
 }
 
 export default App;
