1088d2383f916524b1fd79a59811436702d5a718
Noobsdev2k <99718570+Noobsdev2k@users.noreply.github.com>
2022-09-29 22:42:34 +0700

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const devEnv = process.env.NODE_ENV !== "production";
 
-const { REACT_APP_SERVER_URL } = process.env;
+const { REACT_APP_SERVER_URL, REACT_APP_API_URL } = process.env;
 
 const API = axios.create({
-  baseURL: `${devEnv && REACT_APP_SERVER_URL}`,
+  baseURL: `${devEnv ? REACT_APP_API_URL : REACT_APP_SERVER_URL}`,
 });
 
 API.interceptors.request.use((req) => {
